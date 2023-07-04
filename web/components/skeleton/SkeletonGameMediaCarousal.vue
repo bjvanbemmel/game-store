985c965491714d778e43eedbec1ef9c0b91f1502
bjvanbemmel <beauvanbemmel@live.nl>
2023-07-04 10:57:48 +0200

    The SkeletonGameMediaCarousal is now mobile-friendly as well.

--- web/components/skeleton/SkeletonGameMediaCarousal.vue
+++ web/components/skeleton/SkeletonGameMediaCarousal.vue
@@ -1,10 +1,10 @@
 <template>
-    <SkeletonContainer class="h-[30rem] w-[55rem]" />
-    <div class="flex gap-2 w-[55rem] overflow-x-scroll">
-        <SkeletonContainer class="shrink-0 h-32 w-52" />
-        <SkeletonContainer class="shrink-0 h-32 w-52" />
-        <SkeletonContainer class="shrink-0 h-32 w-52" />
-        <SkeletonContainer class="shrink-0 h-32 w-52" />
-        <SkeletonContainer class="shrink-0 h-32 w-52" />
+    <SkeletonContainer class="h-64 md:h-[30rem] w-full md:w-[55rem]" />
+    <div class="flex gap-2 w-full md:w-[55rem] overflow-x-scroll">
+        <SkeletonContainer class="shrink-0 h-24 md:h-32 w-40 md:w-52" />
+        <SkeletonContainer class="shrink-0 h-24 md:h-32 w-40 md:w-52" />
+        <SkeletonContainer class="shrink-0 h-24 md:h-32 w-40 md:w-52" />
+        <SkeletonContainer class="shrink-0 h-24 md:h-32 w-40 md:w-52" />
+        <SkeletonContainer class="shrink-0 h-24 md:h-32 w-40 md:w-52" />
     </div>
 </template>
